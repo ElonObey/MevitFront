5ad3e0298b94a186be0e63411e829a0047066c8e
d.gulin <dima-0303@yandex.ru>
2024-05-15 11:17:48 +0300

    refactor: add MUI

--- front/src/views/authView.jsx
+++ front/src/views/authView.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import bigLogo from "../assets/bigLogo.svg";
 import "./auth.css";
+import { TextField } from "@mui/material";
 
 function AuthView() {
   return (
@@ -9,6 +10,9 @@ function AuthView() {
         <div className="bigLogo">
           <img src={bigLogo} />
         </div>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField id="standard-basic" label="Standard" variant="standard" />
       </div>
     </>
   );
