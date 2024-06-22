eb2bc4895d91a9f11b59f1b9df5c99b737a273ae
d.gulin <dima-0303@yandex.ru>
2024-06-22 22:59:12 +0300

    chore: style update in module

--- front/src/components/CustomTiles/CustomTiles.jsx
+++ front/src/components/CustomTiles/CustomTiles.jsx
@@ -11,6 +11,33 @@ import {
 import React from "react";
 import CustomCard from "./CustomCard";
 import { Link } from "react-router-dom";
+import image1 from '../../assets/bld.jpg'
+import image2 from '../../assets/14x.jpg'
+import image3 from '../../assets/bld2.png'
+import image4 from '../../assets/krov_3.jpg'
+
+// const CardSettings = [
+//   {
+//     head: "Анализ процесса деформации эритроцитов",
+//     body: "Измерение способности эритроцитов изменять свою форму при воздействии механических сил",
+//     to: "deformsrv",
+//   },
+//   {
+//     head: "Анализ агрегации тромбоцитов",
+//     body: "Исследование, которое проводится для оценки функции тромбоцитов и их способности к слиянию",
+//     to: "plataggsrv",
+//   },
+//   {
+//     head: "Определение эритроцитов и агрегатов",
+//     body: "Регистрация агрегационной активности эритроцитов, подсчёт количества агрегатов эритроцитов во взвеси отмытых эритроцитов в плазме крови",
+//     to: "erythaggsrv",
+//   },
+//   {
+//     head: "Анализ размера частиц",
+//     body: "Процесс визуализации и измерения распределения частиц по размерам",
+//     to: "partsizesrv",
+//   },
+// ];
 
 export default function CustomTiles() {
   return (
@@ -18,17 +45,45 @@ export default function CustomTiles() {
       <Typography variant="h2"> Выбор функции </Typography>
       <Box m={3} display={"flex"} gap={6} flexWrap={"wrap"}>
         <Link style={{ textDecoration: "none" }} to="deformsrv">
-          <CustomCard head={"Анализ процесса деформации эритроцитов"} />
+          <CustomCard
+            body={
+              "Измерение способности эритроцитов изменять свою форму при воздействии механических сил"
+            }
+            head={"Анализ процесса деформации эритроцитов"}
+            img={image1}
+          />
         </Link>
+
         <Link style={{ textDecoration: "none" }} to="plataggsrv">
-          <CustomCard head={"Анализ агрегации тромбоцитов"} />
+          <CustomCard
+            body={
+              "Исследование, которое проводится для оценки функции тромбоцитов и их способности к слиянию"
+            }
+            head={"Анализ агрегации тромбоцитов"}
+            img={image2}
+          />
         </Link>
+
         <Link style={{ textDecoration: "none" }} to="erythaggsrv">
-          <CustomCard head={"Определение эритроцитов и агрегатов"} />
+          <CustomCard
+            body={
+              "Регистрация агрегационной активности эритроцитов, подсчёт количества агрегатов эритроцитов во взвеси отмытых эритроцитов в плазме крови"
+            }
+            head={"Определение эритроцитов и агрегатов"}
+            img={image3}
+          />
         </Link>
+
         <Link style={{ textDecoration: "none" }} to="partsizesrv">
-          <CustomCard head={"Анализ размера частиц"} />
+          <CustomCard
+            body={
+              "Процесс визуализации и измерения распределения частиц по размерам"
+            }
+            head={"Анализ размера частиц"}
+            img={image4}
+          />
         </Link>
+
       </Box>
     </Stack>
   );
