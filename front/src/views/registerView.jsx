d5ab3807dfb944cabceaadb47f70fb92293aef87
d.gulin <dima-0303@yandex.ru>
2024-06-30 18:16:56 +0300

    chore: add alert in registerView

--- front/src/views/registerView.jsx
+++ front/src/views/registerView.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import bigLogo from "../assets/Logo.png";
 import "./auth.css";
-import { Box, Link, Stack, TextField, Typography } from "@mui/material";
+import { Alert, Box, Link, Stack, TextField, Typography } from "@mui/material";
 import { Button } from "@mui/material";
 import PasswordField from "../components/UI/TextFields/PasswordField";
 
@@ -9,17 +9,25 @@ import PasswordField from "../components/UI/TextFields/PasswordField";
 // TODO: декомпозировать
 //
 function RegisterView() {
+  const [alertStatus, setAlertStatus] = React.useState(true)
   return (
     <Stack
       height={"100%"}
       justifyContent={"space-between"}
       alignItems={"center"}
     >
-      <Box width={"30%"} height={"40%"} display={"flex"} justifyContent={'space-around'}alignItems={"center"}>
+      <Box
+        width={"30%"}
+        height={"40%"}
+        display={"flex"}
+        justifyContent={"space-around"}
+        alignItems={"center"}
+      >
         <img src={bigLogo} />
       </Box>
       <Stack sx={{ width: "350px", gap: 5, alignItems: "end" }}>
         <Stack gap={1} width="100%">
+          <Alert severity="info" sx={{display: alertStatus&&'none'}}>Данные отправлены</Alert>
           <Typography>Логин</Typography>
           <TextField id="login" variant="outlined" />
 
@@ -30,7 +38,7 @@ function RegisterView() {
           <PasswordField />
         </Stack>
 
-        <Button sx={{ width: "100%" }} variant="contained" size="large">
+        <Button sx={{ width: "100%" }} variant="contained" onClick={() => setAlertStatus(false)} size="large">
           Зарегестрироваться
         </Button>
       </Stack>
